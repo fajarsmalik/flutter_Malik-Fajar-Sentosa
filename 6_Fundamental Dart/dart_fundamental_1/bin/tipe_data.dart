d6d491108672bcc5a20006ecf6a920baf7d83406
fajarsmalik <fajarsmalik01@gmail.com>
2023-03-27 16:33:32 +0700

    tpdata

---
+++ 6_Fundamental Dart/dart_fundamental_1/bin/tipe_data.dart
@@ -0,0 +1,11 @@
+void main(){
+  int usia = 21;
+  print(usia);
+  double beratBadan = 43.3;
+  print(beratBadan);
+  bool pria = true;
+  print(pria);
+  String nama = 'Malik';
+  print(nama);
+
+}
---
+++ 6_Fundamental Dart/dart_fundamental_1/bin/value.dart
@@ -0,0 +1,4 @@
+void main() {
+  final usia = 27;
+  print(usia);
+}
