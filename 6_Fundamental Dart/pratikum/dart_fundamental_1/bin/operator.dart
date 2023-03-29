a29c5a534e8730c1861f7523571efd39e9cb954c
fajarsmalik <fajarsmalik01@gmail.com>
2023-03-29 14:41:00 +0700

    commit

---
+++ 6_Fundamental Dart/pratikum/dart_fundamental_1/bin/operator.dart
@@ -0,0 +1,15 @@
+void main() {
+var usia = 18.0;
+usia = 19;
+print(usia);
+usia += 2;
+print(usia);
+usia -= 2;
+print(usia);
+usia *= 2;
+print(usia);
+usia /= 2;
+print(usia);
+usia %= 2;
+print(usia);
+}
