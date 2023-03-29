6a7ade172bbf905ac34a96d24f17253b1b4e9d04
fajarsmalik <fajarsmalik01@gmail.com>
2023-03-29 16:14:53 +0700

    commit

---
+++ 6_Fundamental Dart/pratikum/soal_fundamental_dart_1/bin/luas_persegi_panjang.dart
@@ -0,0 +1,9 @@
+double persegiPanjang(double panjang, lebar) {
+  return panjang * lebar;
+}
+
+void main(List<String> arguments) {
+  print('Hasil Perhitungan: ');
+  var data = persegiPanjang(5, 10);
+  print(data);
+}
