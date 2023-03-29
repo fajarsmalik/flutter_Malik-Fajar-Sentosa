ac4c7f2bb3eb57a6ec6afae1d38cae0f9d950484
fajarsmalik <fajarsmalik01@gmail.com>
2023-03-29 14:50:30 +0700

    commit

---
+++ 6_Fundamental Dart/pratikum/soal_fundamental_dart_1/bin/Keliling persegi.dart
@@ -0,0 +1,5 @@
+import 'package:soal_fundamental_dart_1/soal_fundamental_dart_1.dart' as soal_fundamental_dart_1;
+
+void main(List<String> arguments) {
+  print('Hello world: ${soal_fundamental_dart_1.calculate()}!');
+}
---
+++ 6_Fundamental Dart/pratikum/soal_fundamental_dart_1/test/soal_fundamental_dart_1_test.dart
@@ -0,0 +1,8 @@
+import 'package:soal_fundamental_dart_1/soal_fundamental_dart_1.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
