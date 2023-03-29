b6f4d019607c684a7701236686d43096890d3fea
fajarsmalik <fajarsmalik01@gmail.com>
2023-03-29 16:23:48 +0700

    commit

---
+++ 6_Fundamental Dart/pratikum/soal_fundamental_dart_1/bin/keliling_lingkaran.dart
@@ -0,0 +1,10 @@
+double lingkaran(double r) {
+  const phi = 3.14;
+  return 2 * phi * r;
+}
+
+void main(List<String> arguments) {
+  print('Hasil Perhitungan: ');
+  var data = lingkaran(5);
+  print(data);
+}
