7bbca50bcf9d1f52fbbfe58f40e067a467071a24
fajarsmalik <fajarsmalik01@gmail.com>
2023-04-01 15:04:20 +0700

    commit

---
+++ 7_Branching - Looping - Function/pratikum/dart_fundamental_2/bin/break.dart
@@ -0,0 +1,8 @@
+void main(List<String> args) {
+  for (var i = 0; true; i++) {
+    if (i == 20) {
+      break;
+    }
+    print(i);
+  }
+}
---
+++ 7_Branching - Looping - Function/pratikum/dart_fundamental_2/bin/do_while.dart
@@ -0,0 +1,7 @@
+void main(List<String> args) {
+  var i = 100;
+  do {
+    print(i);
+    i++;
+  } while (i < 10);
+}
