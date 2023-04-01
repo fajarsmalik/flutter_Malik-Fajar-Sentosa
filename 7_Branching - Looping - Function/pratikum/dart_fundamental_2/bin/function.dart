d471d06116934db2c6223307ece1b97b6283b733
fajarsmalik <fajarsmalik01@gmail.com>
2023-04-01 15:14:42 +0700

    commit

---
+++ 7_Branching - Looping - Function/pratikum/dart_fundamental_2/bin/function.dart
@@ -0,0 +1,32 @@
+// function
+// void halo() {
+//   print('halo');
+// }
+
+// void main() {
+//   halo();
+//   halo();
+//   halo();
+//   halo();
+//   halo();
+// }
+
+// function parameter
+// void tampil(String teks) {
+//   print(teks);
+// }
+
+// void main() {
+//   tampil('halo');
+//   tampil('selamat');
+// }
+
+//function dengan return
+int jumlah(int a, int b) {
+  return a + b;
+}
+
+void main(List<String> args) {
+  var hasil = jumlah(1, 2);
+  print(hasil);
+}
