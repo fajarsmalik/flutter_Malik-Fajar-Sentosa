74174a9bd148653fbbfd8eb58b501a4af2924f10
fajarsmalik <fajarsmalik01@gmail.com>
2023-04-01 14:50:56 +0700

    commit

---
+++ 7_Branching - Looping - Function/pratikum/dart_fundamental_2/bin/control_flow.dart
@@ -0,0 +1,19 @@
+// void main() {
+//   var usia = 18;
+//   if (usia > 20) {
+//     print('remaja');
+//   } else {
+//     print('tidak memiliki golongan');
+//   }
+// }
+
+void main() {
+  var usia = 38;
+  if (usia < 20) {
+    print('remaja');
+  } else if (usia < 40) {
+    print('dewasa');
+  } else {
+    print('tidak memiliki golongan');
+  }
+}
